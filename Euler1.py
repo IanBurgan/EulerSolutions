41e0f423b1fe6db1e0700e1885624a61439de153
Ian Burgan <burganian@gmail.com>
2016-04-30 23:22:32 -0400

    comments

--- Euler1.py
+++ Euler1.py
@@ -1,7 +1,7 @@
 def main():
     total = 0
 
-    #loop over the numbers under 1000
+    # loop over the numbers under 1000
     for i in range(1000):
         if i % 5 == 0 or i % 3 == 0:
             total += i
--- Euler2.py
+++ Euler2.py
@@ -1,18 +1,18 @@
 def main():
     total = 0
 
-    #first two terms in the sequence
+    # first two terms in the sequence
     i = 1
     j = 2
     # the terms under four million
     while j < 4000000:
-        #add even terms to the total
+        # add even terms to the total
         if i % 2 == 0:
             total += i
         if j % 2 == 0:
             total += j
 
-        #increment the terms
+        # increment the terms
         i = i + j
         j = i + j
 

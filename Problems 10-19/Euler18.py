7980716c1d2b7915709c331fbfc0f9a5253fa5fd
Ian Burgan <burganian@gmail.com>
2016-05-07 16:05:40 -0400

    Problem 18

---
+++ Problems 10-19/Euler18.py
@@ -0,0 +1,39 @@
+def main():
+    triangle = [\
+    [75], \
+    [95, 64], \
+    [17, 47, 82], \
+    [18, 35, 87, 10], \
+    [20, 4, 82, 47, 65], \
+    [19, 1, 23, 75, 3, 34], \
+    [88, 2, 77, 73, 7, 63, 67], \
+    [99, 65, 4, 28, 6, 16, 70, 92], \
+    [41, 41, 26, 56, 83, 40, 80, 70, 33], \
+    [41, 48, 72, 33, 47, 32, 37, 16, 94, 29], \
+    [53, 71, 44, 65, 25, 43, 91, 52, 97, 51, 14], \
+    [70, 11, 33, 28, 77, 73, 17, 78, 39, 68, 17, 57], \
+    [91, 71, 52, 38, 17, 14, 91, 43, 58, 50, 27, 29, 48], \
+    [63, 66, 4, 68, 89, 53, 67, 30, 73, 16, 69, 87, 40, 31], \
+    [4, 62, 98, 27, 23, 9, 70, 98, 73, 93, 38, 53, 60, 4, 23]]
+
+    # loop over the triangle starting at the bottom
+    for i in range(len(triangle) - 1, 0, -1):
+        partsum(triangle[i - 1], triangle[i])
+
+    # Below lines can be uncommented to see the entire triangle
+    # after operating
+
+    # for i in triangle:
+    #     print(i)
+
+    print(triangle[0][0])
+
+# replace each number in a row with the best sum of the possible paths down 1 level
+def partsum(row, row2):
+    for i in range(len(row)):
+        if (row[i] + row2[i]) > (row[i] + row2[i + 1]):
+            row[i] = row[i] + row2[i]
+        else:
+            row[i] = row[i] + row2[i + 1]
+
+main()

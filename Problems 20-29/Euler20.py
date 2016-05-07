da3bcc01b1b30db5b809cbbad9d87b7edf893b0d
Ian Burgan <burganian@gmail.com>
2016-05-07 16:55:04 -0400

    Problem 20

---
+++ Problems 20-29/Euler20.py
@@ -0,0 +1,17 @@
+def main():
+    number = factorial(100)
+    total = 0
+    for i in str(number):
+        total += int(i)
+
+    print(total)
+
+def factorial(x):
+    num = x
+    total = 1
+    while num > 1:
+        total *= num
+        num -= 1
+    return total
+
+main()

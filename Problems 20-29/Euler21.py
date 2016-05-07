97a7cc59e0b3ae2f041bea676689d011170dfb43
Ian Burgan <burganian@gmail.com>
2016-05-07 19:04:16 -0400

    Problem 21

---
+++ Problems 20-29/Euler21.py
@@ -0,0 +1,24 @@
+import math
+
+def main():
+    total = 0
+
+    for i in range(1, 10000):
+        other = sumfactors(i)
+        if i == sumfactors(other):
+            if i != other:
+                total += i + other
+
+    print(total/2)
+
+
+def sumfactors(x):
+    total = 1
+    for i in range(2, int(math.sqrt(x) + 1)):
+        # add all factors found to the total
+        if x % i == 0:
+            total += i
+            total += x // i
+    return total
+
+main()
